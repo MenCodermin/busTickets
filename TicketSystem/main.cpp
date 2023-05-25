2c4d7a5fa074f6950e85fc1a4ecb37b1fb716f8f
Nazarbek Oskonbaev <101890086+MenCodermin@users.noreply.github.com>
2023-05-25 16:59:12 +0300

    Updated main.cpp

--- TicketSystem/main.cpp
+++ TicketSystem/main.cpp
@@ -3,62 +3,84 @@
 
 using namespace std;
 
+// TODO: Implement all functions. Responsible for this is Nazar
+class Payment   
+{
+public:
+    void ProcessPayment();
+    void ValidatePayment();
+};
+
+// TODO: Implement all functions. Responsible for this is Nazar
+class Ticket
+{
+public:
+    void CreateTicket();
+    void PrintTicket();
+    void CancelTicket();    
+};
+
+class Passenger
+{
+public:
+    void RegisterPassenger();
+    void ViewBookingHistory();
+    
+};
+
+
+class Bus
+{
+public:
+    void GetAvailableSeats();
+    void BookSeats();
+};
+// TODO: Implement all functions. Responsible for this is Bilol
+class Route
+{
+public:
+    void GetRouteDetails();
+    void CalculateDistance();
+};
+
+// TODO: Implement all functions. Responsible for this is Bilol
+class Schedule
+{
+public:
+    void GetAvailableSchedules();
+    void UpdateSchedule();
+};
+
 int main() {
     sqlite3* db;
+    Payment pay;
+    Ticket tic;
+    Passenger pas;
+    Bus b;
+    Route r;
+    Schedule sc;
+
     char* errMsg = nullptr;
+    int rc = sqlite3_open("bussys.db",&db);
 
-    // Open a connection to the database
-    int rc = sqlite3_open("bussys.db", &db);
     if (rc != SQLITE_OK) {
-        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
-        return rc;
-    }
-    // Create a table
-    const char* createTableQuery = "CREATE TABLE IF NOT EXISTS MyTable (id INT, name TEXT);";
-    rc = sqlite3_exec(db, createTableQuery, nullptr, nullptr, &errMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
+        cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
         return rc;
     }
 
-    // Insert data into the table
-    const char* insertDataQuery = "INSERT INTO MyTable (id, name) VALUES (1, 'John'), (2, 'Jane');";
-    rc = sqlite3_exec(db, insertDataQuery, nullptr, nullptr, &errMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
-        return rc;
-    }
 
-    // Retrieve data from the table
-    const char* selectDataQuery = "SELECT * FROM MyTable;";
-    rc = sqlite3_exec(
-        db,
-        selectDataQuery,
-        [](void* data, int argc, char** argv, char** columnNames) -> int {
-            for (int i = 0; i < argc; i++) {
-                std::cout << columnNames[i] << ": " << argv[i] << std::endl;
-            }
-            return 0;
-        },
-        nullptr,
-        &errMsg);
 
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
-        return rc;
-    }
     cin.get();
-    // Close the database connection
+
     sqlite3_close(db);
 
     return 0;
 }
 
 
-
-
+void Payment::ProcessPayment()
+{
+    
+}
 
 
