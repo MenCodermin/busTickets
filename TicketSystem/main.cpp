eb0288dcf05833a8fe80477fe7b3c69533a2ce00
Nazarbek Oskonbaev <101890086+MenCodermin@users.noreply.github.com>
2023-06-08 22:57:47 +0300

    Updated version
    Implemented ProceedPayment() function, updated database system,moreover, added new functions. Hope you will understand

--- TicketSystem/main.cpp
+++ TicketSystem/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <vector>
+#include <utility>
 #include "lib/sqlite3.h"
 
 using namespace std;
 
+
+string saveLastName = "";
+
 // TODO: Implement all functions. Responsible for this is Nazar
-class Payment   
+class Payment
 {
 public:
-    void ProcessPayment();
+    void ProcessPayment(sqlite3* db);
     void ValidatePayment();
+
+private:
+    double amount;
+    string cardNo;
+    string cardHolder;
+    string expiricyDate;
+    string cvv;
+    vector<string> paymethod {"card","cash"};
 };
 
 // TODO: Implement all functions. Responsible for this is Nazar
@@ -17,17 +33,23 @@ class Ticket
 public:
     void CreateTicket();
     void PrintTicket();
-    void CancelTicket();    
+    void CancelTicket();
 };
 
+// TODO: Implement all functions. Responsible for this is Nazar
 class Passenger
 {
 public:
-    void RegisterPassenger();
-    void ViewBookingHistory();
-    
-};
+    void RegisterPassenger(sqlite3* db);
+    void ViewBookingHistory(sqlite3* db);
+    int GetPassengerID(sqlite3* db);
 
+private:
+    string fname;
+    string lname;
+    string contact;
+    string email;
+};
 
 class Bus
 {
@@ -35,6 +57,7 @@ public:
     void GetAvailableSeats();
     void BookSeats();
 };
+
 // TODO: Implement all functions. Responsible for this is Bilol
 class Route
 {
@@ -51,7 +74,18 @@ public:
     void UpdateSchedule();
 };
 
-int main() {
+string getCurrentDate()
+{
+    time_t currentTime = time(nullptr);
+    tm* localTime = localtime(&currentTime);
+    char buffer[11];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", localTime);
+
+    return string(buffer);
+}
+
+int main()
+{
     sqlite3* db;
     Payment pay;
     Ticket tic;
@@ -61,15 +95,15 @@ int main() {
     Schedule sc;
 
     char* errMsg = nullptr;
-    int rc = sqlite3_open("bussys.db",&db);
+    int rc = sqlite3_open("bussys.db", &db);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
         return rc;
     }
-
-
-
+    pas.RegisterPassenger(db);
+    pay.ProcessPayment(db);
     cin.get();
 
     sqlite3_close(db);
@@ -77,10 +111,154 @@ int main() {
     return 0;
 }
 
+void Ticket::CreateTicket() {}
 
-void Payment::ProcessPayment()
+void Passenger::RegisterPassenger(sqlite3* db)
 {
-    
+    cout << "Enter passenger first name:" << endl;
+    getline(cin, fname);
+
+    cout << "Enter passenger last name:" << endl;
+    getline(cin, lname);
+
+    cout << "Enter contact information:" << endl;
+    getline(cin, contact);
+
+    cout << "Enter email:" << endl;
+    getline(cin, email);
+
+    string sql = "INSERT INTO Passenger (fname,lname, contact,email) VALUES ('" + fname + "','" + lname + "','" + contact + "','" + email + "');";
+    int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, nullptr);
+
+    if (fname.empty() || lname.empty() || contact.empty() || email.empty())
+    {
+        cout << "Invalid name or contact information. Registration failed." << std::endl;
+        return;
+    }
+
+    if (rc != SQLITE_OK)
+        cerr << "Error inserting passenger details into database: " << sqlite3_errmsg(db) << std::endl;
+    else
+        cout << "Passenger registration successful!" << std::endl;
+    saveLastName = lname;
 }
 
+void Passenger::ViewBookingHistory(sqlite3* db) {}
 
+int Passenger::GetPassengerID(sqlite3* db)
+{
+    string sql = "SELECT passenger_id FROM Passenger WHERE lname = ?;";
+
+    sqlite3_stmt* statement;
+    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &statement, nullptr) != SQLITE_OK)
+    {
+        cerr << "Error preparing statement: " << sqlite3_errmsg(db) << endl;
+        return -1;
+    }
+
+    if (sqlite3_bind_text(statement, 1, saveLastName.c_str(), -1, SQLITE_STATIC) != SQLITE_OK)
+    {
+        cerr << "Error binding parameter: " << sqlite3_errmsg(db) << endl;
+        sqlite3_finalize(statement);
+        return -1;
+    }
+
+    int result = sqlite3_step(statement);
+    if (result != SQLITE_ROW)
+    {
+        if (result == SQLITE_DONE)
+        {
+            cerr << "No matching rows found." << endl;
+        }
+        else
+        {
+            cerr << "Error executing statement: " << sqlite3_errmsg(db) << endl;
+        }
+
+        sqlite3_finalize(statement);
+        return -1;
+    }
+
+    int passengerId = sqlite3_column_int(statement, 0);
+    sqlite3_finalize(statement);
+
+    return passengerId;
+}
+
+// Proceeding payment either by cash or by card
+void Payment::ProcessPayment(sqlite3* db)
+{
+    int PaymentType,rc;
+    string sql;
+    Passenger PasObj;
+    sqlite3_stmt* statement;
+    cout << "Choose the method of the payment:\n"
+         << "1. By Cash\n"
+         << "2. By Card\n"
+         << "3. Go Back" << endl;
+    cin >> PaymentType;
+    cin.ignore();
+
+    switch (PaymentType)
+    {
+    case 1: //Payment By Cash
+        cout << "Enter cash" << endl;
+
+        break;
+    case 2: //Payment By Card
+        cout << "Card Holder:" << endl;
+        getline(cin, cardHolder);
+        cout << "Card number:" << endl;
+        getline(cin, cardNo);
+        cout << "Expericy date:" << endl;
+        getline(cin, expiricyDate);
+        cout << "CVV:" << endl;
+        getline(cin, cvv);
+
+        sql = "INSERT INTO Payment (passenger_id,amount,payment_date,payment_method) VALUES(?,?,?,?);";
+        rc = sqlite3_prepare_v2(db,sql.c_str(), -1, &statement,nullptr);
+        if (rc != SQLITE_OK) {
+            cout << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        rc = sqlite3_bind_int(statement,1,PasObj.GetPassengerID(db));
+        if (rc != SQLITE_OK) {
+            cout << "Failed to bind passenger_id: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        rc = sqlite3_bind_double(statement,2,amount);
+        if (rc != SQLITE_OK) {
+            cout << "Failed to bind amount: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        rc = sqlite3_bind_text(statement,3,getCurrentDate().c_str(), -1, SQLITE_STATIC);
+        if (rc != SQLITE_OK) {
+            cout << "Failed to bind payment_date: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        rc = sqlite3_bind_text(statement,4,paymethod[0].c_str(),-1,SQLITE_STATIC);
+        if (rc != SQLITE_OK) {
+            cout << "Failed to bind payment_method: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        rc = sqlite3_step(statement);
+        if (rc != SQLITE_DONE) {
+            cout << "Failed to proceed the payment: " << sqlite3_errmsg(db) << endl;
+            return;
+        }
+
+        sqlite3_finalize(statement);
+        cout<<"Payment inserted successfully!"<<endl;
+        break;
+    case 3:
+        break;
+    default:
+        cout << "Wrong enter!" << endl;
+        break;
+    }
+}
---
+++ TicketSystem/mysql.sql
@@ -0,0 +1,135 @@
+-- CREATE TABLE passenger (id INT PRIMARY KEY NOT NULL,name TEXT NOT NULL,contact char(10) NOT NULL); 
+-- INSERT INTO passenger(id,name,contact) VALUES (1,'NAZARBEK','5488568555');
+-- DELETE FROM passenger WHERE id = 1;
+-- ALTER TABLE passenger ADD COLUMN email TEXT;
+-- DROP TABLE passenger;
+-- CREATE TABLE passenger (id INT PRIMARY KEY NOT NULL,name TEXT NOT NULL,contact char(10) NOT NULL,email TEXT NOT NULL);
+-- CREATE TABLE bus (bus_id INTEGER PRIMARY KEY,bus_number TEXT,capacity INTEGER);
+-- INSERT INTO bus(bus_id,bus_number,capacity) VALUES(1,'AC1232',35);
+-- SELECT * FROM bus;
+-- DELETE FROM bus WHERE bus_id = 1;
+-- CREATE TABLE Route (route_id INTEGER PRIMARY KEY,source TEXT,destination TEXT);
+-- CREATE TABLE Schedule (
+--     schedule_id INTEGER PRIMARY KEY,
+--     bus_id INTEGER,
+--     route_id INTEGER,
+--     departure_time TEXT,
+--     FOREIGN KEY (bus_id) REFERENCES Bus(bus_id),
+--     FOREIGN KEY (route_id) REFERENCES Route(route_id)
+-- );
+-- CREATE TABLE Ticket (
+--     ticket_id INTEGER PRIMARY KEY,
+--     passenger_id INTEGER,
+--     bus_id INTEGER,
+--     seat_number INTEGER,
+--     journey_date TEXT,
+--     status TEXT,
+--     FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id),
+--     FOREIGN KEY (bus_id) REFERENCES Bus(bus_id)
+-- );
+-- CREATE TABLE Payment (
+--     payment_id INTEGER PRIMARY KEY,
+--     passenger_id INTEGER,
+--     amount REAL,
+--     payment_date TEXT,
+--     payment_method TEXT,
+--     FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id)
+-- );
+
+-- INSERT INTO Bus (bus_id, bus_number, capacity) VALUES (1, 'BUS001', 50);
+-- INSERT INTO Bus (bus_id, bus_number, capacity) VALUES (2, 'BUS002', 40);
+-- INSERT INTO Bus (bus_id, bus_number, capacity) VALUES (3, 'BUS003', 60);
+-- INSERT INTO Route (route_id, source, destination) VALUES (1, 'Girne', 'Lefkosa');
+-- INSERT INTO Route (route_id, source, destination) VALUES (2, 'Lefkosa', 'Famagusta');
+-- INSERT INTO Route (route_id, source, destination) VALUES (3, 'Girne', 'Famagusta');
+
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (1, 1, 1, '09:00', '12:00');
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (2, 2, 2, '10:00', '13:00');
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (3, 3, 3, '11:00', '14:00');
+-- SELECT * FROM Route;
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (1, 1, 1, '09:00', '12:00');
+-- ALTER TABLE Schedule ADD COLLUMN arrival_time TEXT;
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (1, 1, 1, '09:00', '12:00');
+-- SELECT * FROM Schedule;
+-- CREATE TABLE schedule_backup (
+--     schedule_id INTEGER PRIMARY KEY,
+--     bus_id INTEGER,
+--     route_id INTEGER,
+--     departure_time TEXT,
+--     arrival_time TEXT,
+--     FOREIGN KEY (bus_id) REFERENCES Bus(bus_id),
+--     FOREIGN KEY (route_id) REFERENCES Route(route_id)
+-- );
+-- INSERT INTO schedule_backup (schedule_id, bus_id, route_id, departure_time)
+-- SELECT schedule_id, bus_id, route_id, departure_time FROM Schedule;
+-- DROP TABLE Schedule;
+-- ALTER TABLE schedule_backup RENAME TO Schedule;
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (1, 1, 1, '09:00', '12:00');
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (2, 2, 2, '10:00', '13:00');
+-- INSERT INTO Schedule (schedule_id, bus_id, route_id, departure_time, arrival_time) VALUES (3, 3, 3, '11:00', '14:00');
+-- INSERT INTO Ticket (ticket_id, passenger_id, bus_id, schedule_id, seat_number, journey_date) VALUES (1, 1, 1, 1, 'A1', '2023-06-01');
+-- INSERT INTO Ticket (ticket_id, passenger_id, bus_id, schedule_id, seat_number, journey_date) VALUES (2, 2, 1, 2, 'B5', '2023-06-02');
+-- INSERT INTO Ticket (ticket_id, passenger_id, bus_id, schedule_id, seat_number, journey_date) VALUES (3, 3, 2, 1, 'C3', '2023-06-03');
+-- CREATE TABLE passenger_backup AS SELECT * FROM passenger;
+-- DROP TABLE Passenger;
+-- CREATE TABLE Passenger (
+--     passenger_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT NOT NULL,
+--     contact TEXT NOT NULL,
+--     email TEXT NOT NULL
+-- );
+-- INSERT INTO Passenger (name, contact, email)
+-- SELECT name, contact, email
+-- FROM passenger_backup;
+-- DROP TABLE passenger_backup;
+-- INSERT INTO Payment(passenger_id,amount,payment_date,payment_method) VALUES (2,10.0,'10/02/2023','card');
+-- INSERT INTO Passenger(name,contact,email) VALUES ('Igor','8800553535','sa;leka@gmail.com');
+-- DELETE FROM Payment WHERE payment_id = 1;
+-- DELETE FROM Payment WHERE passenger_id = 1;
+-- SELECT * FROM Payment;
+-- SELECT passenger_id FROM Payment WHERE type = 'table' AND sql LIKE '%FOREIGN KEY%';
+-- ALTER TABLE Payment DROP CONSTRAINT fk_passenger_id;
+-- CREATE TABLE Payemnt_backup AS SELECT * FROM Payment;
+-- DROP TABLE Payment;
+-- CREATE TABLE Payment (
+--   payment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   passenger_id INTEGER,
+--   amount DECIMAL,
+--   payment_date TEXT,
+--   payment_method TEXT,
+--   FOREIGN KEY (passenger_id) REFERENCES Passenger (passenger_id)
+-- );
+
+-- INSERT INTO Payment (payment_id,passenger_id,amount,payment_date,payment_method)
+-- SELECT payment_id,passenger_id,amount,payment_date,payment_method
+-- FROM Payemnt_backup;
+
+-- DROP TABLE Payemnt_backup;
+-- SELECT * FROM Payment;
+-- INSERT INTO Payment (passenger_id, amount, payment_date, payment_method)
+-- VALUES (1, 250.00, '2023-05-21', 'Card');
+
+-- ALTER TABLE Passenger RENAME COLUMN name TO fname;
+-- ALTER TABLE Passenger ADD NEW COLLUMN fname TEXT;
+-- ALTER TABLE Passenger ADD CONSTRAINT fk_lname UNIQUE(lname);
+-- CREATE TABLE passenger_backup AS SELECT * FROM Passenger;
+-- DROP TABLE Passenger;
+-- CREATE TABLE Passenger (
+--     passenger_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     fname TEXT NOT NULL,
+--     lname TEXT NOT NULL UNIQUE,
+--     contact TEXT NOT NULL,
+--     email TEXT NOT NULL
+-- );
+-- INSERT INTO Passenger (fname, contact, email)
+-- SELECT fname,contact, email
+-- FROM Passenger_backup;
+-- DROP TABLE Passenger_backup;
+-- SELECT * FROM Passenger;
+-- INSERT INTO Passenger(fname,lname,contact,email) VALUES('Bob','Ferguson','559200245','qwerty@gmail.com');
+-- SELECT * FROM Passenger;
+-- DELETE FROM Passenger WHERE passenger_id = 1;
+-- SELECT * FROM Passenger;
+-- DELETE FROM Passenger WHERE passenger_id = 2;
+-- INSERT INTO Passenger(fname,lname,contact,email) VALUES('Bilol','Makhmudov','5838889977','bilolmakh@gmail.com');
+-- INSERT INTO Passenger(fname,lname,contact,email) VALUES('Nazar','Oskonbaev','5831112233','nazarosk@gmail.com');
