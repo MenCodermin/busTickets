db866e0fc2b10977bcaac4326117f0c705163728
Nazarbek Oskonbaev <101890086+MenCodermin@users.noreply.github.com>
2023-06-24 17:26:53 +0300

    Add files via upload

--- Bus.h
+++ Bus.h
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <set>
+#include <sqlite3.h>
+
+using namespace std;
 
 class Bus
 {
 public:
-    void GetAvailableSeats();
+    std::set<int> GetAvailableSeats(sqlite3* db);
+    int getCurrentBusId(sqlite3* db,string CurrentdepartureTime, string CurrentdepartureDate);
+
     void BookSeats();
+private:
+    int getCurrentBusId(sqlite3* db);
+    string departureTime;
+    string departureDate;
 
 };
